9f9f129081560f6103b63ce67ba60edaf2808621
MARTIJN MADDENS <MARTIJN.MADDENS@vdabcampus.be>
2022-06-02 21:21:40 +0200

    first steps towards standings

--- src/app/components/division-overview/division-overview.component.ts
+++ src/app/components/division-overview/division-overview.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-division-overview',
   templateUrl: './division-overview.component.html',
-  styleUrls: ['./division-overview.component.scss']
+  styleUrls: ['./division-overview.component.scss'],
 })
 export class DivisionOverviewComponent implements OnInit {
+  form: FormGroup = new FormGroup({
+    class: new FormControl(1),
+    division: new FormControl(''),
+  });
 
-  constructor() { }
+  classes: number[] = [1, 2, 3, 4, 5];
+  divisions: string[] = ['A'];
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.form
+      .get('class')
+      .valueChanges.subscribe((value) => console.log(value));
   }
-
 }
---
+++ src/app/models/division.ts
@@ -0,0 +1,4 @@
+export interface Division {
+  class: number;
+  division: string;
+}
