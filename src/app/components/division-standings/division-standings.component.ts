79929aafe9e3354ed6edaf9e2894053867fb0812
MARTIJN MADDENS <MARTIJN.MADDENS@vdabcampus.be>
2022-06-03 22:49:42 +0200

    all features complete?

--- src/app/components/division-standings/division-standings.component.ts
+++ src/app/components/division-standings/division-standings.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { Observable, startWith, switchMap } from 'rxjs';
+import { map, Observable, startWith, switchMap } from 'rxjs';
+import { TeamView } from 'src/app/models/club';
 import { Division, DivisionForm } from 'src/app/models/division';
 import { DataBaseService } from 'src/app/services/database.service';
 
@@ -11,13 +12,43 @@ import { DataBaseService } from 'src/app/services/database.service';
 })
 export class DivisionStandingsComponent implements OnInit {
   @Input()
-  form:FormGroup;
+  form: FormGroup;
 
   division$: Observable<Division>;
 
   constructor(private db: DataBaseService) {}
 
   ngOnInit(): void {
-   this.division$ =  this.form.valueChanges.pipe(startWith({class:'1',division:'A'}),switchMap(form=> this.db.getDivision(form.class+form.division)));
+    this.division$ = this.form.valueChanges.pipe(
+      startWith({ class: '1', division: 'A' }),
+      switchMap((form) => this.db.getDivision(form.class + form.division)),
+      map((overview) => {
+        overview.teams.sort(
+          (a, b) =>
+            b.matchPoints * 100 +
+            b.boardPoints -
+            a.matchPoints * 100 +
+            a.boardPoints
+        );
+        return overview;
+      })
+    );
+  }
+
+  findResult(teamHome: TeamView, teamAway: TeamView): number {
+    if (this.sameTeam(teamAway, teamHome)) return null;
+    const roundAway = teamHome.rounds.find((round) =>
+      this.sameTeam(round.teamHome, teamAway)
+    );
+    if (roundAway) return roundAway.scoreAway;
+    const roundHome = teamHome.rounds.find((round) =>
+      this.sameTeam(round.teamAway, teamAway)
+    );
+    if (roundHome) return roundHome.scoreHome;
+    return null;
+  }
+
+  sameTeam(teamA: TeamView, teamB: TeamView): boolean {
+    return teamA.clubId === teamB.clubId && teamA.id === teamB.id;
   }
 }
