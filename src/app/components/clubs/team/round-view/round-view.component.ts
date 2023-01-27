dd2d4ecbbc31f0575b2dff064088d2568cd18b7c
u99mam <martijn.maddens@acv-csc.be>
2023-01-27 11:18:33 +0100

    adding dates

--- src/app/components/clubs/team/round-view/round-view.component.ts
+++ src/app/components/clubs/team/round-view/round-view.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
 import { Round, TeamView } from 'src/app/models/club';
+import { Dates } from 'src/app/models/dates';
+import { DataBaseService } from 'src/app/services/database.service';
 
 @Component({
   selector: 'app-round-view',
@@ -12,11 +15,13 @@ export class RoundViewComponent implements OnInit {
 
   averageEloHome: number;
   averageEloAway: number;
+  date$: Observable<Date>;
 
   displayedColumnsRound: string[] = ['board', 'white', 'black', 'result'];
-  constructor() {}
+  constructor(private database: DataBaseService) {}
 
   ngOnInit(): void {
+   this.date$ =  this.database.getDates().pipe(map(dates=> dates.dates[this.round.id-1].toDate() ));
      }
 
   sameTeam(teamA: TeamView, teamB: TeamView):boolean{
