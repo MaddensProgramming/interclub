76d78a81c6a80d7ebd8edbb4eff9994d76e81563
Martijn <matte0007@hotmail.com>
2023-10-16 17:48:17 +0200

    fix only call 2 rounds

--- functions/src/frbeGatewayCalls.ts
+++ functions/src/frbeGatewayCalls.ts
@@ -3,15 +3,38 @@ import { PlayerFrbe } from './modelsFRBE';
 import { DivisionFrbe } from './modelsFRBE';
 
 export async function getAllResults(): Promise<DivisionFrbe[]> {
+  const roundsToFetch = [1, 2]; // You can add more rounds to this array as needed
+
   try {
-    const response = await fetch(
-      `https://www.frbe-kbsb-ksb.be/api/v1/interclubs/anon/icseries`
-    );
-    const data = await response.json();
-    return data;
+    // Fetch data for each round and store in an array
+    const allFetchedData: DivisionFrbe[][] = [];
+    for (const round of roundsToFetch) {
+      const response = await fetch(
+        `https://www.frbe-kbsb-ksb.be/api/v1/interclubs/anon/icseries?round=${round}`
+      );
+      const data = await response.json();
+      allFetchedData.push(data);
+    }
+
+    // Initialize the final data with the results of the first round
+    const finalData = allFetchedData[0];
+
+    // Now, let's merge the rounds from all fetched data sets
+    for (let i = 1; i < allFetchedData.length; i++) {
+      finalData.forEach((division, index) => {
+        if (
+          allFetchedData[i][index] &&
+          Array.isArray(allFetchedData[i][index].rounds)
+        ) {
+          division.rounds.push(...allFetchedData[i][index].rounds);
+        }
+      });
+    }
+
+    return finalData;
   } catch (err) {
-    console.error('Error fetching josn', err);
-    return null;
+    console.error('Error fetching JSON', err);
+    throw err;
   }
 }
 export async function getPlayers(clubId: number): Promise<PlayerFrbe[]> {
